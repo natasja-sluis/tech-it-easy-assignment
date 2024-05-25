8e30ae88e654f4d609b11cf4cb30fe1defd9ac90
natasja-sluis <natasjag92@gmail.com>
2024-05-25 14:23:40 +0200

    added sort buttons

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,10 @@ function App() {
     const price = generatePrice(bestSellingTv);
     const availableScreenSizes = generateScreenSize(bestSellingTv);
 
+    function handleClick() {
+        console.log("button clicked!");
+    }
+
     return (
         <>
             <h1>Tech it easy dashboard</h1>
@@ -34,10 +38,21 @@ function App() {
                     <p className="name-best-selling-tv">{nameBestseller}</p>
                     <p className="price-best-selling-tv">{price}</p>
                     <p className="screen-best-selling-tv">{availableScreenSizes}</p>
-                    <p><img className="icons" src="/src/assets/check.png" alt="check-icon"/> wifi <img className="icons" src="/src/assets/minus.png" alt="minus-icon"/> speech
+                    <p><img className="icons" src="/src/assets/check.png" alt="check-icon"/> wifi <img className="icons"
+                                                                                                       src="/src/assets/minus.png"
+                                                                                                       alt="minus-icon"/> speech
                         <img className="icons" src="/src/assets/check.png" alt="check-icon"/> hdr
                         <img className="icons" src="/src/assets/minus.png" alt="minus-icon"/> bluetooth <img
                             className="icons" src="/src/assets/minus.png" alt="minus-icon"/> ambilight </p>
+                </div>
+            </article>
+            <article className="sort-tvs-container">
+                <h2>Alle tvs</h2>
+                <div className="sort-button-section">
+                <button className="sort-button" type="button" onClick={handleClick}>Meest verkocht eerst</button>
+                <button className="sort-button" type="button" onClick={handleClick}>Goedkoopste eerst</button>
+                <button className="sort-button" type="button" onClick={handleClick}>Meest geschikt voor sport eerst
+                </button>
                 </div>
             </article>
         </>
